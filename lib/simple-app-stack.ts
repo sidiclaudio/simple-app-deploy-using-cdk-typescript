aed8c733ab4d887642e602208a0dee71b04ac265
Claudio Sidi <sidiclau@amazon.com>
2021-03-10 00:23:10 -0800

    initial commit

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,9 +1,20 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
+import { CfnOutput } from '@aws-cdk/core';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'ClaudioCdkBucketTest', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+
+    new CfnOutput(this, 'ClaudioCdkBucketTestExport', {
+      value: 'ClaudioCdkBucketTestExport',
+      description: 'a random description',
+      exportName: 'ClaudioCdkBucketTestExportName',
+    });
   }
 }
